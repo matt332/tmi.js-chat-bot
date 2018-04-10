2aaa8bed6bfb823589bca27e60aa852f0b7e1418
matt332 <mathew70988@gmail.com>
2018-04-10 19:38:44 +0100

    Add files via upload
    These are the main files for the bot, but for it to work open the node.js command prompt and type npm i tmi.js --save
    
    DO NOT USE THE PASSWORD AND USER NAME FOUND ON THE FILE

---
+++ index.js
@@ -0,0 +1,71 @@
+var tmi = require('tmi.js');
+
+var options = {
+  options: {
+    debug: true
+  },
+  connection: {
+    cluster: "aws",
+    recconect: true
+  },
+  identity: {
+    username: "matty70988",
+    password: "oauth:a6hgxtwopeqwqzf9po7zoiv8cjjhxh"
+  },
+  channels: ["#fluffyscab"]
+};
+
+var client = new tmi.client(options);
+client.connect();
+
+client.on("chat", function(channel, user, message, self){
+  if(message === "!pony"){
+    client.say("fluffyscab", "https://bit.ly/2JAvusC");
+  };
+});
+
+client.on("chat", function(channel, user, message, self){
+  if(message.startsWith("!pony->")){
+    var input = message.split(" ")[1];
+    client.say("fluffyscab", input + " https://bit.ly/2JAvusC");
+  };
+});
+
+client.on("chat", function(channel, user, message, self){
+  if(message.startsWith("!so")){
+    var input = message.split(' ')[1];
+    if(input.count < 2) return;
+    if(user.mod){
+      client.say("fluffyscab", "Go check out " + input + " at http://twitch.tv/" + input);
+    };
+  };
+});
+
+client.on("chat", function(channel, user, message, self){
+  if (message.startsWith("!donate->")) {
+    var input = message.split(' ')[1];
+    client.say("fluffyscab", input + " To donate go here http://streamlabs.com/fluffyscab");
+  } 
+});
+
+client.on("chat", function(channel, user, message, self){
+  if(message === "!donate"){
+    client.say("fluffyscab", "To donate go here http://streamlabs.com/fluffyscab");
+  };
+});
+
+
+client.on("chat", function(channel, user, message, self){
+  if(message === "!commands"){
+    client.say("fluffyscab", "the current commands are - !pony - !donate - mod only commands - !so (shout out)");
+    client.say("fluffyscab", "To direct a command at someone type -> after the word for example \"!donate->\"");
+  };
+});
+
+client.on("chat", function(channel, user, message, self){
+   if(message.startsWith("!commands->")){
+    var input = message.split(' ')[1];
+    client.say("fluffyscab", input + " the current commands are - !pony - !donate - mod only commands - !so (shout out)");
+    client.say("fluffyscab", "To direct a command at someone type -> after the word for example \"!donate->\"");
+   };
+});
